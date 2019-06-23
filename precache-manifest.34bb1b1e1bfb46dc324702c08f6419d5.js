ab5d0f4d2e486998cc8e2ac7988dbf4daa150b8b
Kishan Kumar <kishan.kumar@intimetec.com>
2019-06-23 23:11:18 +0530

    Updates

---
+++ precache-manifest.34bb1b1e1bfb46dc324702c08f6419d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c439aa15d34caffa8e78e9063aeeab12",
+    "url": "/hello-react-gh/index.html"
+  },
+  {
+    "revision": "6ce63242cf73405476ab",
+    "url": "/hello-react-gh/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/hello-react-gh/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6ce63242cf73405476ab",
+    "url": "/hello-react-gh/static/js/main.c01ff72e.chunk.js"
+  },
+  {
+    "revision": "dfb5ac18ef72266530d4",
+    "url": "/hello-react-gh/static/js/runtime~main.87450e69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-react-gh/static/media/logo.5d5d9eef.svg"
+  }
+]);
